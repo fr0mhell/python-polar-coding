c27189c6a50468df7c9588ab284e5cd25e08eece
grigory <grigory.timofeev@gmail.com>
2019-06-20 22:02:12 +0700

    Add tests for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from utils import int_to_bin_list
+
+
+class UtilsTestCase(TestCase):
+
+    def test_int_to_bin_list(self):
+        len_a = 8
+        a = pow(2, len_a) - 1
+        a_bin = [1 for _ in range(len_a)]
+        self.assertEqual(int_to_bin_list(a, len_a), a_bin)
+
+        len_b = 16
+        b = pow(2, len_b) - 1
+        b_bin = [1 for _ in range(len_b)]
+        self.assertEqual(int_to_bin_list(b, len_b), b_bin)
