70304b0005ab5166b29e4c43f0cc73decaad452d
grigory <grigory.timofeev@saritasa.com>
2020-08-06 10:20:03 +0700

    Add packaging files

--- README.md
+++ README.md
@@ -73,8 +73,25 @@ for snr in snr_range:
 
 ## TODO
 
-[TODO List](TODO.md)
+### General
+
+- [ ] Fix CRC-aided SC List decoder
+
+### Polar code construction
+
+- [ ] Arikan’s Monte-Carlo estimation [Section V.B](https://arxiv.org/pdf/1501.02473.pdf)
+- [ ] Trifonov’s Gaussian approximation [Section V.D](https://arxiv.org/pdf/1501.02473.pdf)
+
+### Decoding
+- [ ] [SC STACK Decoding](https://ieeexplore.ieee.org/document/6215306)
+- [ ] [Fast SSC List Decoding](https://arxiv.org/pdf/1703.08208.pdf)
+- [ ] [Generalized Fast SSC LIST Decoding](https://arxiv.org/pdf/1804.09508.pdf)
+
+### Modulation
+
+- [ ] Q-PSK
+- [ ] 4-QAM
 
 ## License
 
-[MIT License](LICENSE.MD)
+[MIT License](LICENSE.txt)
--- setup.py
+++ setup.py
@@ -1,30 +1,52 @@
 import setuptools
-from pip._internal.download import PipSession
+from pip._internal.network.session import PipSession
 from pip._internal.req import parse_requirements
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 requirements = parse_requirements(
-    filename='requirements/production.txt',
+    filename='requirements.txt',
     session=PipSession()
 )
 
 setuptools.setup(
     name='python-polar-coding',
+
     version='0.1.0',
-    author='Grigory Timofeev',
-    author_email='t1m0feev.grigorij@gmail.com',
+
     description='Polar coding implementation in Python',
+
     long_description=long_description,
+
     long_description_content_type='text/markdown',
+
     url='https://github.com/fr0mhell/python-polar-coding',
-    packages=setuptools.find_packages(),
+
+    author='Grigory Timofeev',
+
+    author_email='t1m0feev.grigorij@gmail.com',
+
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
     ],
-    python_requires='>=3.6',
-    install_requires=[str(requirement.req) for requirement in requirements],
+
+    keywords='polar codes, fec',
+
+    packages=setuptools.find_packages(),
+
+    python_requires='>=3.6, <4',
+
+    install_requires=[req.requirement for req in requirements],
+
+    project_urls={  # Optional
+        'Bug Report': 'https://github.com/fr0mhell/python-polar-coding/issues',
+        'Source': 'https://github.com/fr0mhell/python-polar-coding',
+    },
 )
